7816a5f50a8576a41ce0e0cbb522b43036124b01
Zhou Chang <achang.zhou@gmail.com>
2014-06-09 09:53:38 +0800

    Added android demo
    Signed-off-by: Zhou Chang <achang.zhou@gmail.com>

---
+++ dogs_detection/android/src/com/beginvision/dogdetector/NativeAgent.java
@@ -0,0 +1,16 @@
+package com.beginvision.dogdetector;
+
+import java.io.*; 
+import java.net.*;
+
+import android.net.*;
+import android.util.Log;
+
+public class NativeAgent{
+    /*
+    public static native int updatePicture(String target, byte[]frame, int wid, int hei);
+
+    public static native int updatePictureForResult(String target, 
+                                                    byte[]frame, Object obj, int wid, int hei);
+    */                                               
+}
