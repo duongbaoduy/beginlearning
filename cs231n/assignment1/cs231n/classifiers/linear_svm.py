e4a2848ba0145992fc9e4dbeb8aeddc274419eb4
Zhou Chang <achang.zhou@gmail.com>
2015-02-06 17:20:26 +0800

    SVM none vectorized is really OK
    Signed-off-by: Zhou Chang <achang.zhou@gmail.com>

--- cs231n/assignment1/cs231n/classifiers/linear_svm.py
+++ cs231n/assignment1/cs231n/classifiers/linear_svm.py
@@ -53,7 +53,8 @@ def svm_loss_naive(W, X, y, reg):
         continue
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
-        dW += X[:, i]
+        dW[j, :] += X[:, i] / num_train;
+        dW[y[i], :] -= X[:, i] / num_train;
 
   dW += W * reg;
 
@@ -74,7 +75,7 @@ def svm_loss_vectorized(W, X, y, reg):
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  
+
 
 
   #############################################################################
